e3b12ba20ee6a7727f64a326506c8770b830ff5f
gnaneshkunal <gnaneshpingu@gmail.com>
2017-04-25 01:06:11 +0530

    huge changes

--- src/gameReducer.js
+++ src/gameReducer.js
@@ -1,16 +1,70 @@
+const _ = require('lodash');
+
+const NO_GAMES = '---There are no current Games---';
+const NOT_PART = '---You are not a part of the game---';
+const INVALID = '---Invalid command---';
+
 module.exports = function(game, chunk, socket) {
     switch(true) {
         case chunk === 'iam:':
             if(!game.isActive) {
-                return emit('There are no current Games', socket);
+                return emit(NO_GAMES, socket);
             } else {
-                var aPlater = _.findKey(game, socket);
-                if (aPlater) {
-                    return emit('You are ' + _.findKey(game, socket), socket);
+                var aPlayer = fetchPlayer(game, socket);
+                if (aPlayer) {
+                    return emit('---You are ' + aPlayer + '---', socket);
                 } else {
-                    return emit('You are not a part of the game', socket);
+                    return emit(NOT_PART, socket);
                 }
-            }
+            } break;
+        case chunk.includes('my:'):
+            if (!game.isActive || game.isActive == 'waiting') {
+                return emit(NO_GAMES, socket);
+            } else {
+                var char = fetchPlayer(game, socket);
+                if (!char) {
+                    return emit(NOT_PART, socket);
+                }
+                var xo = chunk.split(':');
+                if (xo.length !== 2) {
+                    return emit(INVALID, socket);
+                }
+                var num = parseInt(xo[1]);
+                xo = (parseInt(xo[1]) >= 0 && parseInt(xo[1]) < 9) ? xo[1] : false;
+                if (xo) {
+                    var character = fetchPlayer(game, socket);
+                    if (!character) {
+                        return emit(NOT_PART, socket);
+                    }
+                    if (game.game[num] === ' ') {
+                        if (game[char].chance){
+                            game.game[num] = character;
+                            alert(board(game));
+                            if (char === 'x') {
+                                game.x.chance = false;
+                                game.o.chance = true; 
+                            } else {
+                                game.x.chance = true;
+                                game.o.chance = false; 
+                            }
+                        } else {
+                            return emit('Please wait for opponent to react', socket);
+                        }
+                    } else {
+                        alert(board(game));
+                        return emit('Please choose another spot', socket);
+                    }
+                } else {
+                    return emit(INVALID, socket);
+                }
+                if (checkWin(game.game)) {
+                    alert('---' + socket.details.name + ' has won the game---');
+                    return game = afterWin(game);
+                } else if (checkFinish(game.game)) {
+                    alert('---Game has ended---');
+                    return game = afterFinish(game);
+                }
+            } break;
         case chunk.includes('game:'):
             if (!game.isActive) {
                 var xo = chunk.split(':');
@@ -18,27 +72,42 @@ module.exports = function(game, chunk, socket) {
                 if (xo) {
                     game[xo] = socket;
                     game.isActive = 'waiting';
-                    return emit('you have selected ' + xo + '\n', socket);
+                    return emit('you have selected ' + xo, socket);
                 }
             } else if (game.isActive == 'waiting') {
                 var xo = chunk.split(':');
                 xo = xo.length === 2 && (xo[1] === 'x' || xo[1] === 'o') ? xo[1] : false;
                 if (xo) {
                     if (game.hasOwnProperty(xo)) {
-                        return emit(xo + ' has been assigned to another player.\n', socket);
+                        return emit(xo + ' has been assigned to another player.', socket);
                     }
                     else {
                         game[xo] = socket;
                         game.isActive = true;
-                        return alert('Game has been started:\n' + '\n--- ' + game.x.details.name + '[x] vs ' + game.o.details.name + '[o] ---\n');
+                        game.game = [];
+                        game[xo].chance = true;
+                        for (var x = 0; x < 9; x++) {
+                                game.game.push(' ');
+                        }
+                        return alert('Game has been started:---\n' + '\n--- ' + game.x.details.name + '[x] vs ' + game.o.details.name + '[o]');
                     }
+                } else {
+                    return emit(INVALID, socket);
                 }
             } else {
                 if (Object.values(game).includes(socket)) {
-                    return emit('You are in the game\n', socket);
+                    return emit('You are in the game', socket);
                 } else {
-                    return emit('You are not in the game\n', socket);
+                    return emit(NOT_PART, socket);
                 }
-            }
+            } break;
+        case chunk.includes('score:'):
+            if (!game.isActive) {
+                return emit('The game is not active.', socket);
+            } else if (game.isActive == 'waiting') {
+                return emit('Waiting for a player to join.', socket);
+            } else {
+                return emit(board(game), socket);
+            } break;
     }
 }
